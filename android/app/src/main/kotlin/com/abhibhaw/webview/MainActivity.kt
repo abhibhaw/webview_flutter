b5d9f8099e28abea6d187e127d8ebf42c837464b
abhibhaw <asthana3110@gmail.com>
2022-09-08 23:14:31 +0530

    feat: Adds webview template

---
+++ android/app/src/main/kotlin/com/abhibhaw/webview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.abhibhaw.webview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:webview/web_view.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Webview Demo',
+      theme: ThemeData.dark(),
+      home: const WebViewApp(),
+    );
+  }
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  final controller = Completer<WebViewController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: WebViewStack(controller: controller)),
+    );
+  }
+}
